8ebc542c60d70998cc50bf29394615a61fa9fe14
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-10 02:46:00 +0300

    add custom transition snackbar

---
+++ src/utils/TransitionSnackbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Alert, Slide, Snackbar } from "@mui/material";
+
+function TransitionSnackbar({open, closeSnackbar, message, severity}) {
+  const [transition, setTransition] = useState(() => TransitionRight);
+
+  function TransitionRight(props) {
+    return <Slide {...props} direction="right" />;
+  }
+
+  // const handleTransition = (Transition) => () => {
+  //   setTransition(() => Transition);
+  //   setOpen(true);
+  // };
+
+  const handleClose = () => {
+    closeSnackbar();
+  };
+
+  return (
+    <Snackbar
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={transition}
+      anchorOrigin={{ horizontal: "center", vertical: "top" }}
+      autoHideDuration={2000}
+    >
+      <Alert
+        onClose={handleClose}
+        severity={severity || "info"}
+        variant="filled"
+        sx={{ width: "100%" }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
+
+export default TransitionSnackbar;

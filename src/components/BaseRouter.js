dbecc7065ed2f3478d801f2bb88dc1e2c4600d4c
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-13 01:28:57 +0300

    fix login loading bug

--- src/components/BaseRouter.js
+++ src/components/BaseRouter.js
@@ -2,27 +2,26 @@ import { fetchUserData } from "features/authSlice";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Redirect, Route, Switch } from "react-router-dom";
+import ApplicationManagement from "screens/ApplicationManagement";
 import Home from "screens/Home";
 import Login from "screens/Login";
 import SignUp from "screens/Signup";
 import PrivateRoute from "./PrivateRoute";
 
 function BaseRouter() {
-
   const dispatch = useDispatch();
 
-  useEffect(()=>{
+  useEffect(() => {
     const token = localStorage.getItem("token");
-    if(token){
-      dispatch(fetchUserData(token))
-    }
-  },[dispatch])
+      dispatch(fetchUserData(token));
+  }, [dispatch]);
 
   return (
     <Switch>
       <Route path="/signup" component={SignUp} />
       <Route path="/login" component={Login} />
       <PrivateRoute path="/" exact component={Home} />
+      <PrivateRoute path="/management" exact component={ApplicationManagement} />
       <Redirect to={"/404"} />
     </Switch>
   );

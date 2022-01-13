af7ddd725e699018d99bb64c5cc7614faa4067cf
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-14 00:29:53 +0300

    create Application Management Table & add column header style

--- src/components/ApplicationManagement/AppManagementTable.js
+++ src/components/ApplicationManagement/AppManagementTable.js
@@ -1,13 +1,41 @@
-import Table from 'components/core/Table';
-import React from 'react'
-import { columns, rows } from "utils/tableColumns/MockColumns";
+import { DeleteOutlineOutlined, Edit, Visibility } from "@mui/icons-material";
+import { Container, IconButton, Switch } from "@mui/material";
+import Table from "components/core/Table";
+import React from "react";
+import { getAppManagementColumns } from "utils/tableColumns/AppManagementColumns";
+
+const applicationColumns = getAppManagementColumns.map((e, i) => ({
+  ...e,
+  ...(e.field === "actions" && {
+    renderCell: () => (
+      <div>
+        <IconButton className="table__icon">
+          <Visibility />
+        </IconButton>
+        <IconButton className="table__icon">
+          <Edit />
+        </IconButton>
+        <IconButton className="table__icon">
+          <DeleteOutlineOutlined />
+        </IconButton>
+        <Switch className="table__switch" />
+      </div>
+    ),
+    renderHeader: () => (
+      <div className="management__actions">
+        <div>Actions</div>
+        <div>View | Edit | Delete | Track</div>
+      </div>
+    ),
+  }),
+}));
 
 function AppManagementTable() {
   return (
-    <div>
-      <Table columns={columns} mockRows={rows} url="" />
-    </div>
-  )
+    <Container style={{ marginBottom: 20, marginTop: 50 }}>
+      <Table columns={applicationColumns} url="api/plant/getAll" />
+    </Container>
+  );
 }
 
-export default AppManagementTable
+export default AppManagementTable;

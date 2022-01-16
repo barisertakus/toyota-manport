1a4e532c76673a6578934ef6da426be568a4d4cd
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2022-01-16 03:23:10 +0300

    update DeleteIssue dialog

--- src/components/application/issueManagement/actions/DeleteAppIssue.js
+++ src/components/application/issueManagement/actions/DeleteAppIssue.js
@@ -7,17 +7,9 @@ function DeleteAppIssue({removeIssue, id}) {
 
   const [open, setOpen] = useState(false);
 
-  const openDialog = () => {
-    setOpen(true);
-  };
-
-  const closeDialog = () => {
-    setOpen(false);
-  };
-
   const handleRemove = () => {
     removeIssue(id);
-    closeDialog();
+    setOpen(false);
   }
 
   return (
@@ -27,14 +19,14 @@ function DeleteAppIssue({removeIssue, id}) {
       setOpen={setOpen}
       dialogProps={{ maxWidth: "sm", fullWidth: true }}
       controlButton={
-        <IconButton className="table__icon" onClick={openDialog}>
+        <IconButton className="table__icon" onClick={() => setOpen(true)}>
           <DeleteOutlineOutlined />
         </IconButton>
       }
     >
       <div>Are you sure you want to remove the issue?</div>
       <div className="remove__buttons">
-        <Button color="primary" variant="contained" onClick={closeDialog}>
+        <Button color="primary" variant="contained" onClick={() => setOpen(false)}>
           CANCEL
         </Button>
         <Button color="error" variant="contained" onClick={handleRemove}>
